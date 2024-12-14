e0fbbdfaea98423b6f421c9d759d73dc108994ff
PocoPota <pocopota.program@gmail.com>
2024-12-14 23:02:01 +0900

    処理結果の受取処理記述部分追加

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,18 @@
 <script>
+  export let form;
 </script>
 
 <header>
   <div>Spotics</div>
 </header>
 <main>
-  <form method="post" enctype="multipart/form-data">
+  <form method="post" enctype="multipart/form-data" use:enhance>
     <input type="file" name="file" />
     <button>Upload</button>
   </form>
-  
+  {#if form?.success}
+    <!-- ここに成功時の処理を記述 -->
+  {:else}
+    <!-- ここに失敗時の処理を記述 -->
+  {/if}
 </main>
